8fc3d074dd904e9df090e3674c060429049d575b
tlenormand <lenormand.thomas76110@gmail.com>
2021-09-30 09:13:10 +0000

    part 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,13 @@
 /* Print the first comment in c */
 
 int main(void)
+
+	/* Print without use printf */
 {
+
 char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
 
 	return (0);
+
 }
