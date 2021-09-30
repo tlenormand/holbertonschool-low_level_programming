ea66e27d2eccea47a96b469b52ed8d6429d15428
tlenormand <lenormand.thomas76110@gmail.com>
2021-09-30 11:30:47 +0000

    Correct the spelling again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int_type));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int_type));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_type));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int_type));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
