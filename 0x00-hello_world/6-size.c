33c80b7b076fd15144942d4153bad9808255964f
tlenormand <lenormand.thomas76110@gmail.com>
2021-09-30 11:27:07 +0000

    Correct short type and long type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ int main(void)
 	char char_type;
 
 	/* sizeof evaluates the size of a variable */
-	printf("Size of a char: %zu bytes\n", sizeof(char_type));
-	printf("Size of a int: %zu bytes\n", sizeof(int_type));
-	printf("Size of a long int: %zu bytes\n", sizeof(long_int_type));
-	printf("Size of a long long int: %zu byte\n", sizeof(long_long_int_type));
-	printf("Size of a float: %zu bute\n", sizeof(float_type));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
+	printf("Size of a int: %zu byte(s)\n", sizeof(int_type));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_type));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int_type));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
 
 	return (0);
 }
