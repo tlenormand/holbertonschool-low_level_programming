159986519adbb8dd679563eb7d1ca46cca2f88d8
tlenormand <lenormand.thomas76110@gmail.com>
2021-09-30 09:09:12 +0000

    part 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-char str[] = "Programming is like building a multilingual puzzle";
+char str[] = "Programming is like building a multilingual puzzle\n";
 	puts(str);
 
 	return (0);
