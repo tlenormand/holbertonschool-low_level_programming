16ac947694c223efd40eed747f7d5279d97cf511
tlenormand <lenormand.thomas76110@gmail.com>
2021-09-30 10:15:59 +0000

    part 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the first comment in c using printf
+ *
+ * Return: 0 always for main
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+
+}
+
