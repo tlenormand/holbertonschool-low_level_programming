17b19a168d9fbd1fe6ac303b151c4f2a9491fa84
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-01 15:28:41 +0000

    correct for to while - part 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,22 +10,29 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	num1 = 0;
+	num2 = num1 + 1;
+	while (num1 <= 98)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		while (num2 <= 99)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
 			putchar(32);
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
+			if (num1 != 98 && num2 != 99)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			else
+			{
 
-			if (num1 == 98 && num2 == 99)
-				continue;
-
-			putchar(44);
-			putchar(32);
+			}
+			num2++;
 		}
+		num1++;
 	}
 
 	putchar('\n');
