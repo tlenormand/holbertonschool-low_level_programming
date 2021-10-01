2b48270fcff6a0c96ec5562f095f560723e3f8c8
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-01 10:38:52 +0000

    part 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - write digit number
+ * write all single digit numbers of base 10 starting from 0, using putchar
+ * Return: 0 always
+ */
+
+int main(void)
+{
+
+int i = 48;
+
+while (i < 58)
+{
+	putchar(i);
+	putchar(44);
+	putchar(32);
+	i++;
+}
+
+putchar('\n');
+
+return (0);
+}
