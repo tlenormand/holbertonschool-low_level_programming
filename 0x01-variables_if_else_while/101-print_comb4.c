4b407b97502b249deed3c9f1c20e274d052d9b33
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-01 14:32:20 +0000

    correct the betty style - part 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,45 +8,42 @@
 
 int main(void)
 {
-
 int c = 48;
 int d = 49;
 int u = 50;
-
 	while (c < 58)
 	{
 		while (d < 58)
 		{
 			while (u < 58)
 			{
-
 				if (c == d || d == u || u == c)
 				{
 
 				}
 				else
 				{
-                    putchar(c);
-				    putchar(d);
-				    putchar(u);
-                    if (c == 55 && d == 56 && u == 57)
-                    {
+					putchar(c);
+					putchar(d);
+					putchar(u);
+					if (c == 55 && d == 56 && u == 57)
+					{
 
-                    }
-                    else
-                    {
-					putchar(44);
-					putchar(32);
-                    }
+					}
+					else
+					{
+						putchar(44);
+						putchar(32);
+					}
 				}
-                u++;
+				u++;
 			}
-            u = d + 1;
-            d++;
+			u = d + 1;
+			d++;
 		}
-        d = c + 1;
-        c++;
-    }
+		d = c + 1;
+		c++;
+	}
 
 putchar('\n');
 
