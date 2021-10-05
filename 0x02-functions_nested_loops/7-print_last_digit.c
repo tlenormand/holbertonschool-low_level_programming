20653df6d36c40c981ab34c114a5b6a4d9dc24f4
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-05 12:51:17 +0000

    correct the minimum int - part 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,11 @@
 int print_last_digit(int n)
 {
 
-int last_digit;
+int last_digit = n % 10;
 
-if (n < 0)
-	n = n * -1;
+if (last_digit < 0)
+	last_digit *= -1;
 
-last_digit = n % 10;
 _putchar(last_digit + '0');
 
 return (last_digit);
