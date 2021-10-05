5beb015b2657f8817ddc108de5527ba6547d0d25
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-05 18:45:48 +0000

    change output remove some print of fibonacci number - part 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,23 +9,23 @@
 int main(void)
 {
 
-int i = 1;
+int i = 0;
 unsigned long int fib1 = 0;
 unsigned long int fib2 = 1;
 unsigned long int c = 1;
 
-while (i < 97)
+while (i < 95)
 {
+	if (i == 0)
+		printf("%lu", c);
+	else
+		printf(", %lu", c);
 	fib2 = fib1;
 	fib1 = c;
 	c = fib1 + fib2;
-	printf("%lu, ", c);
 	i++;
 }
-fib2 = fib1;
-fib1 = c;
-c = fib1 + fib2;
-printf("%lu\n", c);
 
+printf("\n");
 return (0);
 }
