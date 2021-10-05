b1c3f58a96ba9e3c644a19652515f20a89744597
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-05 12:09:46 +0000

    part 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - xxx
+ *
+ * @n: varaible test
+ *
+ * Return: xxx
+ */
+
+int print_last_digit(int n)
+{
+
+int last_digit;
+
+last_digit = n % 10;
+
+return (last_digit);
+}
