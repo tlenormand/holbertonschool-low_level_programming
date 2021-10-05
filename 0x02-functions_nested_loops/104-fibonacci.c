c20580b2e88610b47c1d0da9577dc4fac039d18f
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-05 18:37:18 +0000

    Correction output - part 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,22 +13,16 @@ int i = 1;
 unsigned long int fib1 = 0;
 unsigned long int fib2 = 1;
 unsigned long int c = 1;
-int cpt2 = 1;
 
-while (i <= 1000)
+while (i < 98)
 {
+	printf("%lu, ", c);
 	fib2 = fib1;
 	fib1 = c;
 	c = fib1 + fib2;
-	if (c % 2 == 0)
-	{
-	cpt2 += 1;
-	if (cpt2 != 98)
-		printf("%lu, ", c);
-	}
 	i++;
 }
 printf("%lu\n", c);
-printf("%d", cpt2);
+
 return (0);
 }
