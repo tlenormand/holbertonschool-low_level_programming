c90aff3c554f1e94279bde807d4e997760e2a217
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-05 13:06:30 +0000

    part 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - xxx
+ *
+ * Return: xxx
+ */
+
+void jack_bauer(void)
+{
+
+int i = 0;
+int j = 0;
+
+while (i < 24)
+{
+	while (j < 60)
+	{
+		_putchar((i / 10) + '0');
+		_putchar((i % 10) + '0');
+		_putchar(58);
+		_putchar((j / 10) + '0');
+		_putchar((j % 10) + '0');
+		_putchar('\n');
+		j++;
+	}
+	j = 0;
+	i++;
+}
+
+}
