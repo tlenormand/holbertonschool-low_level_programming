11f1b6110a307e71f6e574ca5fea8fe75f38ef88
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-06 16:58:03 +0000

    update main.h, update file and betty - part 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,6 @@
 #include <stdio.h>
-#ifndef MAIN_H
-#define MAIN_H
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int, int);
 void print_remaining_days(int, int, int);
-#endif /* MAIN_H */
