32ddd478d122d5938360b0c3c0aa3ee9f18d93b2
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-06 11:50:20 +0000

    add positive_or_negative file function - part 0

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,26 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
 /**
- * main - print whether the number in the variable n is positive or negative.
+ * positive_or_negative - print whether the number in the variable n is positive or negative.
  *
  * Return: 0 always
  */
 
-/* betty style doc for function main goes there */
 int positive_or_negative(int n)
 {
 
-	srand(time(0));
-	/*n = rand() - RAND_MAX / 2;*/
-/* your code goes there */
 if (n > 0)
-printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 if (n == 0)
-printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 if (n < 0)
-printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
+
 return (0);
 }
