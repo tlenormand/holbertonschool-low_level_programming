7bbc9aa696abe7a5bfcb1158aca63541d5f712b6
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-11 12:18:57 +0000

    add modulo - part 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,9 @@ void puts2(char *s)
 
 int i;
 
-	for (i = 0; s[i] != '\0'; i += 2)
-		_putchar(s[i]);
+	for (i = 0; s[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(s[i]);
 	_putchar('\n');
 
 }
