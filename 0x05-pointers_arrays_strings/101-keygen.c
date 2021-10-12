991aa00337c4da0e17398937290ea126fec29eb4
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-12 16:58:17 +0000

    part 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generate a random password
+ *
+ * Return: 0 always
+ */
+
+
+int main(void)
+{
+	int total = 2772;
+	char password;
+
+	srand(time(NULL));
+	while (total > 126)
+	{
+		password = rand() % 126;
+		printf("%c", password);
+		total -= password;
+	}
+	printf("%c", total);
+
+	return(0);
+}
