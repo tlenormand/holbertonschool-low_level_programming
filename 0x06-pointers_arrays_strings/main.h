bd8b69775a3451fc0e0f04a8e0b79237082ad834
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-14 21:51:42 +0000

    part 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,3 +10,5 @@ char *leet(char *);
 char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+int _strlen(char *s);
+char *add_if_size_array_different(char *r, int i, int ten, int len_n, int nb_n2);
