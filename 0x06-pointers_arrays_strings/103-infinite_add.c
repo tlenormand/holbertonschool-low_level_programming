412145cc92549093d47f76e0717defbbb6bf1ef4
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-15 21:34:28 +0000

    correct output - part 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,42 +1,49 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * infinite_add - function that prints an integer
- * @n1: integer to print
- * @n2: integer to print
- * @r: integer to print
- * @size_r: integer to print
- * Return: xxx
+ * infinite_add - Add 2 inputed number into string with a limit
+ * size on the output
+ *
+ * @n1: First number
+ * @n2: Second number
+ * @r: Output array
+ * @size_r: Size of the output array
+ *
+ * Return: 0 if there is a problem, else the sum
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 
-int l1 = _strlen(n1), l2 = _strlen(n2), i = 0, ten = 0;
-int nb_n1, nb_n2, k, len_r, temp, left;
-	for (l1 -= 1, l2 -= 1; l1 >= 0 && l2 >= 0; l1--, l2--, i++)
-	{
-		nb_n1 = n1[l1] - '0';
-		nb_n2 = n2[l2] - '0';
+	int len1, len2, strlen, i = 0, ten = 0, sum = 0, len_r, temp, left;
 
-		if (nb_n1 + nb_n2 + ten >= 10)
-			r[i] = (nb_n1 + nb_n2 + ten) % 10 + '0';
-		else
-			r[i] = nb_n1 + nb_n2 + ten + '0';
-		ten = (nb_n1 + nb_n2 + ten) / 10;
+/*la chaine la plus longue*/
+	len1 = _strlen(n1);
+	len2 = _strlen(n2);
+	if (len1 > len2)
+		strlen = len1;
+	else
+		strlen = len2;
+
+/*si chaine dépasse le size_r sort du programme*/
+	if (strlen + 1 >= size_r)
+		return (0);
+
+/*somme des deux tableaux*/
+	for (i = 0, len1 -= 1, len2 -= 1; i < size_r - 1; i++, len1--, len2--)
+	{
+		sum = ten;
+		if (len1 >= 0)
+			sum += n1[len1] - '0';
+		if (len2 >= 0)
+			sum += n2[len2] - '0';
+		if (len1 <= 0 && len2 <= 0 && sum == 0)
+			break;
+		r[i] = sum % 10 + '0';
+		ten = (sum / 10);
+		r[i + 1] = '\0';
 	}
-r[i] = '\0';
-if (l2 >= 0)
-	r = add_if_size_array_different(r, i, ten, l2, nb_n2);
-if (l1 >= 0)
-	r = add_if_size_array_different(r, i, ten, l1, nb_n1);
-for (k = 0; r[k]; k++)
-	i = k;
-if (ten == 1)
-	r[i + 1] = '1', i++;
-if (_strlen(r) + 1 > size_r)
-	return (0);
-r[i + 1] = '\0';
+
 len_r = _strlen(r) - 1;
 		for (left = 0; left <= len_r; left++, len_r--)
 	{
@@ -44,7 +51,8 @@ len_r = _strlen(r) - 1;
 		r[left] = r[len_r];
 		r[len_r] = temp;
 	}
-return (r);
+
+	return (r);
 }
 
 /**
@@ -62,73 +70,4 @@ int _strlen(char *s)
 		strlen++;
 
 	return (strlen);
-}
-
-/**
- * reverse_array - function that reverses the content of an array of integers
- * @a: array to reverse
- * @n: lenght of the "a" array
- */
-
-void reverse_array(int *a, int n)
-{
-
-	int temp, left;
-
-	for (left = 0; left < n; left++, n--)
-	{
-		temp = a[left];
-		a[left] = a[n - 1];
-		a[n - 1] = temp;
-	}
-
-}
-
-/**
- * _strcpy - function that copies the string pointed to by src
- * @dest: value to return
- * @src: string to copy
- * Return: return value: the pointer to dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	dest[i] = src[i];
-
-	return (dest);
-}
-
-/**
- * add_if_size_array_different - function add if size of array is different
- * @r: xxxn
- * @i: xxx
- * @ten: xxx
- * @len_n: xxx
- * @nb_n: xxx
- * Return: r
- */
-
-char *add_if_size_array_different(char *r, int i, int ten, int len_n, int nb_n)
-{
-while (len_n >= 0)
-{
-	if (nb_n % 10 + ten > 9)
-	{
-		r[i] = ((nb_n + ten) % 10) + '0';
-		ten = 1;
-	}
-	else
-	{
-		r[i] = ((nb_n + ten) % 10) + '0';
-		ten = 0;
-	}
-	i++;
-	len_n--;
-}
-return (r);
 }
