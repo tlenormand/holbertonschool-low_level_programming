ad078b9b389c68c3a4a8063649c81f93ddd8017e
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-18 10:31:06 +0000

    part 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: string to be controlled
+ * @accept: charactere to control in s
+ * Return: number of bytes in the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int len = 0;
+
+	if ((*s == '\0') || (*accept == '\0'))
+		return (len);
+
+	while (*s && _strchr(accept, *s++))
+	{
+		len++;
+	}
+	return (len);
+}
+
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string to find
+ * @c: charactere to dearch
+ * Return: first occurence of c in s or 0 if not fund
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0' && *s != c)
+		++s;
+	return ((*s != '\0' || c == '\0') ? s : 0);
+}
