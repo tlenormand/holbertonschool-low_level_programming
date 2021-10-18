09219b6d532800b50d07fa5a0f18f91d883e8211
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-18 10:27:51 +0000

    part 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string to find
+ * @c: charactere to dearch
+ * Return: first occurence of c in s or 0 if not fund
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0' && *s != c)
+		++s;
+	return ((*s != '\0' || c == '\0') ? s : 0);
+}
