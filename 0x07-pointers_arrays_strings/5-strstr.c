013fcc992ff16f99d094417e251515603184663e
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-18 19:32:34 +0000

    part 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,15 +9,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	unsigned int i = 0, j = 0;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-		for (j = 0; needle[j] != '\0'; j++)
+	while (*(haystack + i) != '\0')
+	{
+		while (*(haystack + i + j) == *(needle + j) && *(needle + j) != '\0')
+			j++;
+		if (*(needle + j) != '\0')
 		{
-			if (haystack[i + j] != needle[j])
-				break;
-			if (!needle[j])
-				return (haystack + i);
+			j = 0;
+			i++;
 		}
+		if (*(needle + j) == '\0')
+			return (haystack + i);
+	}
+
 	return (0);
 }
+
