8d5276d1c9178acf6bd42cac228721ffb602ea50
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-26 12:17:19 +0000

    update comments - part 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,9 +2,9 @@
 #include "main.h"
 
 /**
- * free_grid - xxx
- * @grid: xxx
- * @height: xxx
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: grid to free
+ * @height: size of the height
  */
 
 void free_grid(int **grid, int height)
