1f1a9906aa739c56818aac29c956093c25795a21
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-27 16:10:38 +0000

    correct all code - part 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -18,55 +18,43 @@ char **strtow(char *str)
 
 /*count the words of the array "str" to assign in row in "list"*/
 	while (str[i])
-	{
-		if (str[i] == ' ' && str[i + 1] != ' ')
-			row++, i++, len = 0;
+		if (str[i] != ' ')
+			row++, i += nb_letter(i, str);
 		else
 			i++;
-	}
 
 /*assign the size "row" for the array "list"*/
-	list = (char **)malloc((sizeof(char *) * row));
+	list = (char **)malloc((sizeof(char *) * row + 1));
 	if (list == NULL)
 	{
 		free(list);
 		return (NULL);
 	}
 
-/*assign the size "len" of the array "list[row]" and the the value*/
 	i = 0, len = 0, row = 0;
 	while (str[i])
 	{
-		/*skip if space*/
-		if (str[i] == ' ' && str[i + 1] == ' ' && str[i + 1] != '\0')
-			i++;
-		/*this is a new word, so a new column to declare in malloc with*/
-		/*the lenght of the word then reset the lenght*/
-		else if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		if (str[i] != ' ')
 		{
-			list[row] = (char *)malloc((1 + nb_letter(i + 1, str) * sizeof(char)));
+			list[row] = (char *)malloc((1 + nb_letter(i, str) * sizeof(char)));
 			if (list[row] == NULL)
 			{
 				for (list_index = 0; list_index <= row; list_index++)
 					free(list[list_index]);
 				free(list);
-				printf("error");
 				return (NULL);
 			}
-			row++, i++, len = 0;
-		}
-		/*otherwise, you don't have space so duplicate str in list[row][lenght]*/
-		else if (str[i] != ' ')
-		{
-			list[row - 1][len] = str[i];
-			len++, i++;
+			len = 0;
+			for (list_index = i; str[list_index] != ' ' && str[list_index]; list_index++)
+			{
+				list[row][len] = str[list_index];
+				len++, i++;
+			}
+			row++;
 		}
 		else
 			i++;
 	}
-
-	if (row == 0)
-		return (NULL);
 
 	return (list);
 }
@@ -86,5 +74,5 @@ int nb_letter(int i, char *str)
 	while (str[i] != ' ' && str[i] != '\0')
 		letter++, i++;
 
-	return(letter);
+	return (letter);
 }
