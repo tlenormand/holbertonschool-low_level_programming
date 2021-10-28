52ba589b8edee559968ca931076d1f645bb4c049
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-28 12:00:18 +0000

    correct memory allocated - part 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	list = (int *)malloc(1 + (max - min + 1) * (sizeof(int)));
+	list = (int *)malloc(1 + (max - min) * (sizeof(int)));
 	if (list == NULL)
 	{
 		free(list);
