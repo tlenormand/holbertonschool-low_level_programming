afab862b9b9fd8b127fbe8f05818295c79f68237
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-28 11:54:37 +0000

    correct memory allocation - part 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,20 +18,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	strlen_s2 = s2 == NULL ? 0 : _strlen(s2);
 	strlen_s2 = n >= strlen_s2 ? strlen_s2 : n;
 
-	list = (char *)malloc(sizeof(char) * (strlen_s1 + strlen_s2 + 1));
+	list = (char *)malloc(sizeof(char) * (strlen_s1 + strlen_s2 + 6));
 	if (list == NULL)
 	{
-		/*free(list);*/
+		free(list);
 		return (NULL);
 	}
 
 	for (i = 0; i < strlen_s1; i++)
-	{
 		*(list + i) = *(s1 + i);
-	}
 
-	for (i = 0; i < n && i < strlen_s2; i++)
-		*(list + strlen_s1 + i) = *(s2 + i);
+	for (i = 0; i < strlen_s2; i++)
+		*(list + (strlen_s1 + i)) = *(s2 + i);
 
 	return (list);
 }
