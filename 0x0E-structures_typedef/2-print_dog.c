fea38cd77b76cf46a490c46f78df0631c64fd015
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-31 18:40:06 +0000

    correct 6 float for age - part 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: (nil)\n");
 		if ((*d).age != 0 && d->age)
-			printf("Age: %f\n", d->age);
+			printf("Age: %6f\n", d->age);
 		else
 			printf("Age: (nil)\n");
 		if ((*d).owner != NULL)
