cb9f4173fc02f63d0467a4b11cadd4f5d8b507bf
tlenormand <lenormand.thomas76110@gmail.com>
2021-10-31 18:47:02 +0000

    correct general code - part 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,19 +8,19 @@
 
 void print_dog(struct dog *d)
 {
-	if (d != NULL)
-	{
-		if (d->name != NULL)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
-		if ((*d).age != 0 && d->age)
-			printf("Age: %6f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-		if ((*d).owner != NULL)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n");
-	}
+	if (d == NULL)
+		return;
+
+	if (d->name != NULL)
+		printf("Name: %s\n", d->name);
+	else
+		printf("Name: (nil)\n");
+	if (d->age != 0 && d->age)
+		printf("Age: %6f\n", d->age);
+	else
+		printf("Age: (nil)\n");
+	if (d->owner != NULL)
+		printf("Owner: %s\n", d->owner);
+	else
+		printf("Owner: (nil)\n");
 }
