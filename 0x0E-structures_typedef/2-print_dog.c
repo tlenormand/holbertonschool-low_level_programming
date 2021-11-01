43551a70b10f0604058ccec095ab735252616ce9
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-01 20:29:02 +0000

    correct age - part 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,9 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name);
 	else
 		printf("Name: (nil)\n");
-	if (d->age != 0 && d->age)
-		printf("Age: %6f\n", d->age);
-	else
-		printf("Age: (nil)\n");
+
+	printf("Age: %6f\n", d->age);
+
 	if (d->owner != NULL)
 		printf("Owner: %s\n", d->owner);
 	else
