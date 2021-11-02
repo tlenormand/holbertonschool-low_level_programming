3880c68676430fec21e72d0635d6faf1faee635c
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-02 09:18:45 +0000

    add malloc for parameters - part 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,21 +13,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
 
-	my_dog = malloc(sizeof(dog_t) + 1);
-	if (my_dog == NULL)
+	my_dog = malloc(sizeof(dog_t));
+
+	my_dog->name = malloc(sizeof(name) + 1);
+	(*my_dog).name = name;
+	if (my_dog->name == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
 
-	(*my_dog).name = name;
-	if ((*my_dog).name == NULL)
-	{
-		free(my_dog->name);
-		free(my_dog);
-		return (NULL);
-	}
 	my_dog->age = age;
+
+	my_dog->owner = malloc(sizeof(owner) + 1);
 	(*my_dog).owner = owner;
 	if ((*my_dog).owner == NULL)
 	{
