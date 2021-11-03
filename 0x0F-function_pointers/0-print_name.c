92580dba9cc31a4d3a2271024d276239508f9b0b
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-03 11:41:01 +0000

    add if name == NULL - part 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: function print sentence and name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	f(name);
+}
