4389c6249d8545e93eaf36c8c225e03b5acec2d2
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-03 12:12:00 +0000

    add condition NULL - part 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size != 0)
+	if (action != NULL && array != NULL)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
