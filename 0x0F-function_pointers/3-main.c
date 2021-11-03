fa8b3d1f3435f2db16246fd7b4da412f99bf74d6
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-03 15:32:22 +0000

    add if double sign exit 99 - part 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,6 +19,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
+	if (strlen(argv[2]) > 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
 	return (0);
