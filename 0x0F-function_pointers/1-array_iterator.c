e3e2ed2790dd77800592cf9e9d2c8c27f1077bf6
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-03 12:07:06 +0000

    add if size == 0 - part 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	if (size != 0)
+		for (i = 0; i < size; i++)
+			action(array[i]);
 }
