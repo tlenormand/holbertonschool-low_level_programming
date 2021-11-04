5f0dca999e9bec150535736e6b2552db8f9f17bc
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-04 18:14:57 +0000

    part 4

---
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -0,0 +1,18 @@
+bits 64
+
+section .data
+	message db 'Hello World', 10
+
+section .text
+	global main
+
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 11+1
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
