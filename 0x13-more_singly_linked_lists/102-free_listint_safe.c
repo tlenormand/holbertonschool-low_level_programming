6626022f137f5600764ea9161e4229065bf00f18
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-16 22:53:14 +0000

    remove if loop exist - part 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,22 +9,23 @@
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *next_node;
-    listint_t *tmp;
-    int size = 0, index = 0;
+	listint_t *tmp;
+	int number_of_node = 0, index = 0;
 
-    next_node = *h;
-    tmp = *h;
-	while (next_node && detect_loop(*h, next_node, index) != 0)
+	next_node = *h;
+
+	while (next_node)
 	{
-        tmp = next_node;
-        printf("[%p] %d\n", (void *)tmp, tmp->n);
-        next_node = next_node->next;
-        size += sizeof(tmp);
+		tmp = next_node;
+		printf("[%p] %d\n", (void *)tmp, tmp->n);
+		next_node = next_node->next;
+		tmp->next = NULL;
 		free(tmp);
-        index++;
-
+		number_of_node++;
+		index++;
 	}
-    *h = NULL;
 
-    return (size);
+	*h = NULL;
+
+	return (number_of_node);
 }
