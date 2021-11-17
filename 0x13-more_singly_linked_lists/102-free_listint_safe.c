0d9eb3f61cb0573defef30200b30794d07bf5ea2
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-17 09:36:55 +0000

    remove printf - part 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -10,19 +10,17 @@ size_t free_listint_safe(listint_t **h)
 {
 	listint_t *next_node;
 	listint_t *tmp;
-	int number_of_node = 0, index = 0;
+	int number_of_node = 0;
 
 	next_node = *h;
 
-	while (next_node)
+	while (next_node->next)
 	{
 		tmp = next_node;
-		printf("[%p] %d\n", (void *)tmp, tmp->n);
 		next_node = next_node->next;
 		tmp->next = NULL;
 		free(tmp);
 		number_of_node++;
-		index++;
 	}
 
 	*h = NULL;
