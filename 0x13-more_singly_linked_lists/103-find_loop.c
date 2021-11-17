323034e69faa7707e1cb726d13d00814540ad600
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-17 12:43:02 +0000

    correct code according to part 13 - part 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -2,17 +2,17 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - function that prints a listint_t linked list
+ * find_listint_loop - function that finds the loop in a linked list
  * @head: list address
- * Return: number of node finded
+ * Return: address of the node where the loop starts, or NULL if no loop
  *
  * Description: check if endless listint_t linked list exist
  */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-	const listint_t *curr_node = head;
-	int number_of_nodes = 0;
+	listint_t *curr_node = head;
+	size_t number_of_nodes = 0;
 
 	while (curr_node)
 	{
@@ -20,8 +20,7 @@ listint_t *find_listint_loop(listint_t *head)
 		curr_node = curr_node->next;
 		if (detect_loop(head, curr_node, number_of_nodes) != 0)
 		{
-			printf("Loop starts at [%p] %d\n", (void *)curr_node, curr_node->n);
-			return ((void *)curr_node);
+			return (curr_node);
 		}
 	}
 
