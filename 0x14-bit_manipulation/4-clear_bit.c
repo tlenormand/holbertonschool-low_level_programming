8242943c7fc74246040ddf53c5be7b572712746a
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-18 16:04:10 +0000

    part 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * @n: number to change the bit
+ * @index: position of the bit to get
+ * Return: 1 if success, 0 othewise
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index <= 32)
+	{
+		*n &= *n - (1 << index);
+		return (1);
+	}
+
+	return (-1);
+}
+
