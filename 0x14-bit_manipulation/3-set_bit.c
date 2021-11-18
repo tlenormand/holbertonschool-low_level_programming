e7c7ac3966acf7780b703e463ae7b4e4e8c08bb3
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-18 15:32:19 +0000

    correct indew <= 31 and betty - part 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,15 +2,17 @@
 #include <stdio.h>
 
 /**
- * @brief 
- * 
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * @n: number to change the bit
+ * @index: position of the bit to get
+ * Return: 1 if success, 0 othewise
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int *number = n;
 
-	if (index <= 32)
+	if (index <= 31)
 	{
 		*number = ((*number >> index));
 		if (((*number >> 0) & 1) == 0)
