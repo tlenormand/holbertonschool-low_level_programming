bb9537960ed18300e8a8549453c24b5cb2a1d8fa
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 13:15:26 +0000

    correct if file doesn't exist - part 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,9 +14,12 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
+	if (!(open(filename, O_WRONLY)))
+	{
 	create_return = creat(filename, S_IRUSR | S_IWUSR);
-	if (create_return == -1)
-		return (-1);
+		if (create_return == -1)
+			return (-1);
+	}
 
 	fd = open(filename, O_WRONLY);
 	if (fd == -1)
