2943757f2b5bdf6cde6da52f6a0e6ff529663c95
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 22:50:12 +0000

    forgot

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,7 @@ void cp_content(const char *file_from, const char *file_to)
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
