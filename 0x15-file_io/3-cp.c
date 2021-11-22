1421735be2a4b29fb5324c85b7d27502d9d79e17
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 19:26:57 +0000

    correct on error STDERR_FILENO - part 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
@@ -32,30 +32,30 @@ int cp_content(const char *file_from, const char *file_to)
 	char buf[1024];
 
 	if (file_from == NULL)
-		dprintf(2, "Error: Can't read from file %s\n", file_from), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from), exit(98);
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", file_from), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from), exit(98);
 	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(2, "Error: Can't read from file %s\n", file_to), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_to), exit(98);
 
 	while ((read_return = read(fd_from, buf, 1024)) != 0)
 	{
 		if (read_return == -1)
-			dprintf(2, "Error: Can't read from file %s\n", file_from), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from), exit(98);
 		write_return = write(fd_to, buf, read_return);
 		if (write_return == -1)
-			dprintf(2, "Error: Can't close fd %s\n", file_to), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", file_to), exit(99);
 	}
 
 	close_return = close(fd_from);
 	if (close_return == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	close_return = close(fd_to);
 	if (close_return == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 
 	return (0);
 }
