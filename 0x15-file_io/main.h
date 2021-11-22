3e78d7140703ccff2a646c76d9e818910ffc70a6
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 22:27:16 +0000

    add main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 /*function creates in exercices*/
-int cp_content(const char *file_from, const char *file_to);
+void cp_content(const char *file_from, const char *file_to);
 void _close(int fd);
 
 
