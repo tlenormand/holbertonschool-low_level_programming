6f686f7e169577304257b98d22044cacf9d0280e
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 13:25:47 +0000

    correct create - part 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,15 +11,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, create_return, write_return, close_return;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	if (!(open(filename, O_WRONLY)))
-	{
-	create_return = creat(filename, S_IRUSR | S_IWUSR);
-		if (create_return == -1)
-			return (-1);
-	}
+	create_return = creat(filename, 0600);
+	if (create_return == -1)
+		return (-1);
 
 	fd = open(filename, O_WRONLY);
 	if (fd == -1)
