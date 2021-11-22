d7c060d8e60f41f0cb7ccb65fd1d025607d881e8
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 22:37:21 +0000

    correct STDERR_FILENO in main - part 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -64,7 +64,7 @@ void _close(int fd)
 int main(int argc, char **argv)
 {
 	if (argc != 3)
-		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	cp_content(argv[1], argv[2]);
 
