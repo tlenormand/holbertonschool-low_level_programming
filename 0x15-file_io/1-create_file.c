a0218abb9e9ae34bff84e5ffcfb3d601a5d54e85
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 12:58:57 +0000

    add if text_content == NULL - part 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, new, write_return;
 
-	if (filename == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (-1);
 
 	new = creat(filename, S_IRUSR | S_IWUSR);
