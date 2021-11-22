81609edaac92bd01d94c44a38631667d6143002a
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 13:39:05 +0000

    correct if text_content == NULL - part 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,11 +19,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	write_return = write(fd, text_content, _strlen(text_content));
-	if (write_return == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		write_return = write(fd, text_content, _strlen(text_content));
+		if (write_return == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close(fd);
