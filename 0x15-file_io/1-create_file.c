81bf5cf45fafeb65045f5dadd2038c97470cd343
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 13:38:07 +0000

    correct if text_content == NULL - part 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,11 +22,14 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	write_return = write(fd, text_content, _strlen(text_content));
-	if (write_return == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		write_return = write(fd, text_content, _strlen(text_content));
+		if (write_return == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	close_return = close(fd);
