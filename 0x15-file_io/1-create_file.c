12ed662e9f5c94f29d28153c620ad3efc1320e66
tlenormand <lenormand.thomas76110@gmail.com>
2021-11-22 13:07:59 +0000

    correct open with create - part 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,16 +9,12 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, new, write_return;
+	int fd, write_return;
 
 	if (filename == NULL || text_content == NULL)
 		return (-1);
 
-	new = creat(filename, S_IRUSR | S_IWUSR);
-	if (new == -1)
-		return (-1);
-
-	fd = open(filename, O_WRONLY);
+	fd = open(filename, O_CREAT, 0600);
 	if (fd == -1)
 		return (-1);
 
