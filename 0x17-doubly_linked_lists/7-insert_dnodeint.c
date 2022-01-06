4742c48070730f08a28abceb3397b679b1fd1fbd
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-06 15:44:03 +0000

    test with Frank's code - part 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,44 +1,69 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * insert_dnodeint_at_index - function inserts a new node at a given position
- * @h: pointer to the dlistint_t list
- * @idx: position to insert
- * @n: value
- * Return: the address of the new node, or NULL if it failed
+ * create_node - Create a new node for dlistint_t list
+ *
+ * @n: The element of the node
+ *
+ * Return: new node or NULL if the allocation failed
  */
+dlistint_t *create_node(int n)
+{
+	dlistint_t *new;
 
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	return (new);
+}
+
+/**
+ * insert_dnodeint_at_index - insert a new node at the index
+ * of a dlistint_t list.
+ *
+ * @h: The first node of a dlistint_t list.
+ * @idx: The index where the node is insert
+ * @n: Element of the new node
+ *
+ * Return: The new node, or NULL if the allocation of memory failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = NULL;
-	dlistint_t *ptr = *h;
+	dlistint_t *current = *h;
+	unsigned int current_idx = 0;
 
-	if (!h)
+	if (h == NULL)
 		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx > 0)
+	while (current->next != NULL && current_idx < idx)
 	{
-		ptr = ptr->next;
-		idx--;
-		if (!ptr && idx == 0)
-			return (add_dnodeint_end(h, n));
-		else if (!ptr && idx != 0)
-			return (NULL);
+		current = current->next;
+		current_idx++;
 	}
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
+	if (current_idx + 1 == idx && current->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-	new->n = n;
-	new->prev = ptr->prev;
-	new->next = ptr;
+	if (current_idx == idx)
+	{
+		new = create_node(n);
+		if (new == NULL)
+			return (NULL);
 
-	ptr->prev->next = new;
-	ptr->prev = new;
+		new->prev = current->prev;
+		new->next = current;
+		current->prev->next = new;
+		current->prev = new;
+	}
 
 	return (new);
 }
