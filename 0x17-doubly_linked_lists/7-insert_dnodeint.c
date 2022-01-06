a6d0f1bfa3cec10601935fa9545d8b69a42c742b
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-06 15:17:04 +0000

    correct assignation - part 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx > 1)
+	while (idx > 0)
 	{
 		ptr = ptr->next;
 		idx--;
@@ -27,15 +27,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 
+	if (ptr->next == NULL)
+		return (add_dnodeint_end(h, n));
+
 	new->n = n;
-	new->prev = *h;
-	new->next = ptr->next;
-	ptr->next = new;
+	new->prev = ptr->prev;
+	new->next = ptr;
+
+	ptr->prev->next = new;
+	ptr->prev = new;
 
 	if (ptr->prev != NULL)
 		ptr->next->prev = new;
-	else
-		ptr->prev = NULL;
 
 	return (new);
 }
