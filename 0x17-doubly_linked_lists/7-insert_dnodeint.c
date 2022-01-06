2fb49c2446c7facb402b2911fa9c1175e45eae52
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-06 14:50:42 +0000

    correct function - part 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,11 +13,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 	dlistint_t *ptr = *h;
 
-	if (new == NULL)
+	if (new == NULL || (*h == NULL && idx != 0))
+	{
+		printf("yro");
 		return (NULL);
+	}
 
-	if (*h == NULL || idx == 0)
+	if (*h == NULL && idx == 0)
+	{
+		printf("yoe");
 		return (add_dnodeint(h, n));
+	}
 
 	while (idx > 1)
 	{
@@ -28,9 +34,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	new->n = n;
-	new->prev = *h;
+	new->prev = ptr;
 	new->next = ptr->next;
 	ptr->next = new;
+
 	if (ptr->prev != NULL)
 		ptr->next->prev = new;
 	else
