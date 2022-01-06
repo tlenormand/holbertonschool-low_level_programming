2d252b53e00240650ec766fbb0637296af051af0
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-06 14:13:15 +0000

    correct if list == NULL - part 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 
-	if (idx == 0)
+	if (*h == NULL || idx == 0)
 		return (add_dnodeint(h, n));
 
 	while (idx > 1)
