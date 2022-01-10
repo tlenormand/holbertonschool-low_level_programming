06f6dc8da510b56b7912db296bf2ee8af0408001
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-10 12:11:19 +0000

    part 0, 1, 3

---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,10 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+int _add(int a, int b);
+int _sub(int a, int b);
+int _mul(int a, int b);
+int _div(int a, int b);
+int _mod(int a, int b);
+
+#endif /* OPERATIONS_H */
