6dabba1a49683e1241b4b2a042f30551e98bc16e
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-10 12:13:03 +0000

    correct to *.c compilation - part 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc _atoi.c _isdigit.c _isupper.c _memset.c _puts.c _strchr.c _strcpy.c _strncat.c _strpbrk.c _strstr.c _abs.c _isalpha.c _islower.c _memcpy.c _putchar.c _strcat.c _strcmp.c _strlen.c _strncpy.c _strspn.c -c -fPIC
+gcc *.c -c -fPIC
 gcc *.o -shared -o liball.so
