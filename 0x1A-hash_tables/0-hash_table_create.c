342ece8d1801aeb94217d4c87604d6cdeb7f1243
tlenormand <lenormand.thomas76110@gmail.com>
2022-01-28 09:13:56 +0000

    correct if size == 0 - part 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
 
-	if (hash_table == NULL or size == 0)
+	if (hash_table == NULL || size == 0)
 		return (NULL);
 
 	hash_table->size = size;
